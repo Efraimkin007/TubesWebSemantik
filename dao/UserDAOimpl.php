89d93d19933e6890fd65220e131f9589e438f2c3
Efraimkin007 <69674425+Efraimkin007@users.noreply.github.com>
2021-01-23 19:40:55 +0700

    Change

---
+++ dao/UserDAOimpl.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class UserDAOimpl
+{
+    public function login($user) {
+        $link = PDOUtil::createConnection();
+        $query = "SELECT * FROM user WHERE username = ? AND password = ?";
+        $stmt = $link->prepare($query);
+        $stmt->bindValue(1,$user->getUsername());
+        $stmt->bindValue(2,$user->getPassword());
+        $stmt->setFetchMode(PDO::FETCH_OBJ);
+        $stmt->execute();
+        PDOUtil::closeConnection($link);
+        return $stmt->fetchObject('User');
+    }
+}
--- index.php
+++ index.php
@@ -6,7 +6,29 @@
 <body align="center">
 <h1> Tugas Besar Web Semantik - RSS</h1>
 <h2> Dari Kelompok : Efraim, Mathias, Sehat</h2>
+
+<input class="w3-input w3-round-large" type="text"  placeholder="Masukan Data Judul RSS" required="">
+<input class="w3-input w3-round-large" type="text"  placeholder="Masukan Data Link RSS" required="">
+
+<input class="w3-button w3-blue w3-round-large" type="submit" Value="Tambahkan Data" name="btnSubmit"/>
 <table border="1" align="center">
+
+<?php 
+/*
+$xml = simplexml_load_file("http://rss.cnn.com/rss/edition_asia.rss");
+
+foreach($xml->channel->item as $DATA)
+{
+	echo "<div class='w3-panel w3-padding-16'></br>";
+	echo "<h2 >".$DATA->title." "."</br></h2>";
+	echo $DATA->description." "."<br/>";
+	echo "<br/>";
+	echo "<a href='$DATA->link' class='w3-btn w3-black w3-round-large'>Kunjungi Website</a>";
+	echo "<br/></div>";
+}
+*/
+?>
+
 <th>
 No
 </th>
