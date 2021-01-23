2ef5f8b066f463fea6b16137acb7fa2f7a6b878d
Efraimkin007 <69674425+Efraimkin007@users.noreply.github.com>
2021-01-23 20:16:21 +0700

    ~

--- index.php
+++ index.php
@@ -1,6 +1,30 @@
+<?php
+session_start();
+include_once "util/db_util.php";
+include_once "db_function/rss_link_function.php";
+
+?>
+
 <!DOCTYPE html>
+<html>
 <head>
+
+  <meta charset = "utf-8" />
+  <link rel = "icon" href = "gambar/logo_maranatha.gif">
+
+  <link rel="stylesheet" href="css/pmkmaranatha_css.css">
+
+  <!-- tablestyle!-->
+  <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css"/>
+
+  <script type="text/javascript" src="js/functional_js.js"></script>
 </head>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta content="text/html; charset=iso-8859-2" http-equiv="Content-Type">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
 <body align="center">
